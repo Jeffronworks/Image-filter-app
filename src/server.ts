dff957157bfa27956bd2b538e4f2c68f784435c8
Jeffron <jeffroneu@gmail.com>
2022-06-15 08:28:17 +0100

    added the filtered image endpoint

--- src/server.ts
+++ src/server.ts
@@ -13,28 +13,28 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  //filteredImage enpoint
+  app.get( "/filteredimage", async ( req, res ) => {
+    //get url from the request
+    const {image_url} = req.query 
 
-  /**************************************************************************** */
+    //validate input    
+    if (!image_url) {
+      return res.status(400).send("Please input the image url")
+    }
+    //call filterImageFromURL(image_url) to filter the image
+    const imagePath  = await filterImageFromURL(image_url)
 
-  //! END @TODO1
-  
- 
-  // Displays a simple message to the user
-
-  // Start the Server
+    return  await res.status(200).sendFile(imagePath, err => {
+      if (err) {
+        console.log(err)
+      }
+     // deletes any files on the server on finish of the response
+      const localfiles : string[] = [imagePath]
+      deleteLocalFiles(localfiles)
+    })
+        
+  } );
 
    // Root Endpoint.
   app.get("/", (req , res ) => {
